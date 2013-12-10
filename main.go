28d0484326c4ec0fc04ee0b24534f6cd16c8df74
Peter Waller <p@pwaller.net>
2013-12-10 17:16:34 +0000

    Code gardening

--- main.go
+++ main.go
@@ -22,35 +22,40 @@ func main() {
 	github_user := os.Getenv("GITHUB_USER")
 	github_password := os.Getenv("GITHUB_PASSWORD")
 
-	json := `{ "name": "web",
-                   "config": {"url": "http://services.scraperwiki.com/hook",
-                              "content_type": "json"},
-	          "events": ["push", "issues", "issue_comment",
-                             "commit_comment", "create", "delete",
-                             "pull_request", "pull_request_review_comment",
-                             "gollum", "watch", "release", "fork", "member",
-                             "public", "team_add", "status"],
-                  "active": true
-                 }`
+	json := `{
+	"name": "web",
+	"config": {"url": "http://services.scraperwiki.com/hook",
+		"content_type": "json"},
+	"events": ["push", "issues", "issue_comment",
+		"commit_comment", "create", "delete",
+		"pull_request", "pull_request_review_comment",
+		"gollum", "watch", "release", "fork", "member",
+		"public", "team_add", "status"],
+	"active": true
+	}`
 
-	Endpoint := "https://" + github_user + ":" + github_password + "@" + "api.github.com"
+	endpoint := "https://" + github_user + ":" + github_password + "@" + "api.github.com"
 
 	repos := []string{
 		"scraperwiki/custard",
-		"scraperwiki/tang"}
+		"scraperwiki/tang",
+	}
+
 	for _, repo := range repos {
 
 		buffer := strings.NewReader(json)
-		resp, err := http.Post(Endpoint+"/repos/"+repo+"/hooks", "application/json", buffer)
+		resp, err := http.Post(endpoint+"/repos/"+repo+"/hooks", "application/json", buffer)
 		check(err)
 
 		log.Println("Rate Limit:", resp.Header["X-Ratelimit-Remaining"][0])
+
 		switch resp.StatusCode {
 		default:
 			response, err := ioutil.ReadAll(resp.Body)
 			check(err)
 
 			log.Print(string(response))
+
 		case 422:
 			log.Println("Already hooked for", repo)
 		}
@@ -63,14 +68,18 @@ func main() {
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
+
 	request, err := ioutil.ReadAll(r.Body)
 	check(err)
+
 	var dst bytes.Buffer
 	r.Header.Write(&dst)
 	log.Println("Incoming request headers: ", string(dst.Bytes()))
+
 	dst.Reset()
 	err = json.Indent(&dst, request, "", "  ")
 	check(err)
+
 	var doc_ interface{}
 	err = json.Unmarshal(dst.Bytes(), &doc_)
 	check(err)
@@ -80,10 +89,12 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	switch eventType := r.Header["X-Github-Event"][0]; eventType {
 	case "push":
+
 		log.Println("Pushed")
 		ref := doc["ref"].(string)
 		url := doc["repository"].(map[string]interface{})["url"].(string)
 		after := doc["after"].(string)
+
 		log.Println("Push to", url, ref, "after", after)
 
 	default:
