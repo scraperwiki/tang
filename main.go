92defbb44c90abafbcb32eb4238e391bef5cba64
Peter Waller <peter@scraperwiki.com>
2013-12-12 18:30:13 +0000

    Update the github status endpoint

--- main.go
+++ main.go
@@ -17,17 +17,23 @@ import (
 	"syscall"
 )
 
-var address = flag.String("address", ":8080", "address to listen on")
-var repositories = flag.String("repositories", "scraperwiki/tang", "colon separated list of repositories to watch")
-var allowedPushers = flag.String("allowed-pushers", "drj11:pwaller", "list of people allowed")
+var (
+	address        = flag.String("address", ":8080", "address to listen on")
+	repositories   = flag.String("repositories", "scraperwiki/tang", "colon separated list of repositories to watch")
+	allowedPushers = flag.String("allowed-pushers", "drj11:pwaller", "list of people allowed")
 
-var allowedPushersSet = map[string]bool{}
+	github_user, github_password string
+
+	allowedPushersSet = map[string]bool{}
+)
 
 func init() {
 	flag.Parse()
 	for _, who := range strings.Split(*allowedPushers, ":") {
 		allowedPushersSet[who] = true
 	}
+	github_user = os.Getenv("GITHUB_USER")
+	github_password = os.Getenv("GITHUB_PASSWORD")
 }
 
 func check(err error) {
@@ -63,14 +69,38 @@ func main() {
 	check(err)
 }
 
+func Endpoint(args ...string) string {
+	base := "https://" + github_user + ":" + github_password + "@" + "api.github.com"
+	log.Println("base =", base)
+	return path.Join(append([]string{base}, args...)...)
+}
+
+func Github(payload string, endpoint ...string) (respString string, resp *http.Response, err error) {
+	if os.Getenv("TANG_TEST") != "" {
+		// don't touch the endpoint during tests.
+		return
+	}
+
+	url := Endpoint(endpoint...)
+	resp, err = http.Post(url, "application/json", strings.NewReader(payload))
+	check(err)
+
+	response, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	log.Println("Querying ", url)
+	log.Println("Rate Limit:", resp.Header["X-Ratelimit-Remaining"][0])
+
+	return string(response), resp, err
+}
+
 func configureHooks() {
 
 	if *repositories == "" {
 		return
 	}
-
-	github_user := os.Getenv("GITHUB_USER")
-	github_password := os.Getenv("GITHUB_PASSWORD")
 
 	json := `{
 	"name": "web",
@@ -84,25 +114,15 @@ func configureHooks() {
 	"active": true
 	}`
 
-	endpoint := "https://" + github_user + ":" + github_password + "@" + "api.github.com"
-
 	repos := strings.Split(*repositories, ":")
 
 	for _, repo := range repos {
-		log.Print("Repo: ", repo)
-
-		buffer := strings.NewReader(json)
-		resp, err := http.Post(endpoint+"/repos/"+repo+"/hooks", "application/json", buffer)
+		response, resp, err := Github(json, "repos", repo, "hooks")
 		check(err)
-
-		log.Println("Rate Limit:", resp.Header["X-Ratelimit-Remaining"][0])
 
 		switch resp.StatusCode {
 		default:
-			response, err := ioutil.ReadAll(resp.Body)
-			check(err)
-
-			log.Print(string(response))
+			log.Print(response)
 
 		case 422:
 			log.Println("Already hooked for", repo)
@@ -254,10 +274,39 @@ func gitCheckout(git_dir, checkout_dir, ref string) (err error) {
 	return
 }
 
-func runTang(path, ref string) (err error) {
+type GithubStatus struct {
+	State       string `json:"state"`
+	TargetUrl   string `json:"target_url"`
+	Description string `json:"description"`
+}
+
+// curl -d '{"state": "success", "target_url": "https://deleteme.pwaller.qa.scraperwiki.com/", "description": "Tests pass, deleteme.pwaller.qa.scraperwiki.com up"}' https://scraperwiki-salt:fe236ee1c96a3fcc5dd14047b3d35651ff9b919d@api.github.com/repos/scraperwiki/custard/statuses/c8e3e81f13ce73de42364fd61aae1216b5dc2b69
+func updateStatus(repo, sha string, githubStatus GithubStatus) {
+	bytes, err := json.Marshal(githubStatus)
+	check(err)
+	Github(string(bytes), "repos", repo, "statuses", sha)
+}
+
+func runTang(repo, sha, path, ref string) (err error) {
+
 	cmd := Command(path, "./tang.hook")
 	cmd.Env = append(os.Environ(), "TANG_REF="+ref)
-	return cmd.Run()
+	cmderr := cmd.Run()
+
+	if cmderr == nil {
+		// All OK, send along a green
+		updateStatus(repo, sha, GithubStatus{"success", "http://services.scraperwiki.com", "All OK"})
+
+		return
+	} else if err, ok := err.(*exec.ExitError); ok {
+		status := err.Sys().(syscall.WaitStatus).ExitStatus()
+		_ = status
+		// Not OK, send along red.
+		updateStatus(repo, sha, GithubStatus{"failure", "http://services.scraperwiki.com", "All OK"})
+
+	}
+
+	return
 }
 
 func eventPush(event PushEvent) (err error) {
@@ -276,32 +325,38 @@ func eventPush(event PushEvent) (err error) {
 
 	ref := event.Ref
 	url := event.Repository.Url
-	after := event.After
+	sha_after := event.After
 
-	log.Println("Push to", url, ref, "after", after)
+	gh_repo := path.Join(event.Repository.Organization, event.Repository.Name)
+	status := GithubStatus{"pending", "http://services.scraperwiki.com", "Running"}
+	updateStatus(gh_repo, sha_after, status)
+
+	log.Println("Push to", url, ref, "after", sha_after)
 
 	// The name of the subdirectory where the git
 	// mirror is (or will appear, if it hasn't been
 	// cloned yet).
-	git_dir := path.Join(GIT_BASE_DIR, event.Repository.Organization,
-		event.Repository.Name)
+	git_dir := path.Join(GIT_BASE_DIR, gh_repo)
 	err = gitLocalMirror(url, git_dir)
 	if err != nil {
 		return
 	}
 
-	checkout_dir := path.Join("checkout", after)
-	err = gitCheckout(git_dir, checkout_dir, after)
+	checkout_dir := path.Join("checkout", sha_after)
+	err = gitCheckout(git_dir, checkout_dir, sha_after)
 	if err != nil {
 		return
 	}
 	log.Println("Created", checkout_dir)
 
 	if event.NonGithub.NoBuild {
+		// Bail out. This is here so that the tests can avoid running
+		// themselves.
 		return
 	}
 
-	err = runTang(path.Join(git_dir, checkout_dir), event.Ref)
+	repo_workdir := path.Join(git_dir, checkout_dir)
+	err = runTang(gh_repo, sha_after, repo_workdir, event.Ref)
 	if err != nil {
 		return
 	}
