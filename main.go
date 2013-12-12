f26291b7d3f821606668f1b1b91d783b8f05b7b3
Peter Waller <peter@scraperwiki.com>
2013-12-12 14:09:35 +0000

    Refactoring, tests

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -77,53 +78,86 @@ func configureHooks() {
 }
 
 type Repository struct {
-	Url string `json:"url"`
+	Name         string `json:"name"`
+	Url          string `json:"url"`
+	Organization string `json:"organization"`
 }
 
 type PushEvent struct {
-	Ref        string `json:"ref"`
-	Repository Repository
-	After      string `json:"after"`
+	Ref        string     `json:"ref"`
+	Repository Repository `json:"repository"`
+	After      string     `json:"after"`
 }
 
-func handleHook(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!\n")
+func handleEvent(eventType string, document []byte) (err error) {
 
-	request, err := ioutil.ReadAll(r.Body)
-	check(err)
+	log.Println("Incoming request:", string(document))
 
-	var dst bytes.Buffer
-	r.Header.Write(&dst)
-	log.Println("Incoming request headers: ", string(dst.Bytes()))
-
-	dst.Reset()
-	err = json.Indent(&dst, request, "", "  ")
-	check(err)
-	data := dst.Bytes()
-
-	// var doc_ interface{}
-	// err = json.Unmarshal(dst.Bytes(), &doc_)
-	// check(err)
-	// doc := doc_.(map[string]interface{})
-
-	log.Println("Incoming request:", string(data))
-
-	switch eventType := r.Header["X-Github-Event"][0]; eventType {
+	switch eventType {
 	case "push":
 		var event PushEvent
-		err = json.Unmarshal(data, &event)
-		check(err)
+		err = json.Unmarshal(document, &event)
+		if err != nil {
+			return
+		}
 
-		eventPush(event)
+		err = eventPush(event)
+		if err != nil {
+			return
+		}
 
 	default:
 		log.Println("Unhandled event:", eventType)
 	}
 
+	return
 }
 
-func eventPush(event PushEvent) {
-	log.Println("Pushed")
+func handleHook(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK\n")
+
+	request, err := ioutil.ReadAll(r.Body)
+	check(err)
+
+	var buf bytes.Buffer
+	r.Header.Write(&buf)
+	log.Println("Incoming request headers: ", string(buf.Bytes()))
+
+	buf.Reset()
+	err = json.Indent(&buf, request, "", "  ")
+	check(err)
+
+	eventType := r.Header["X-Github-Event"][0]
+	data := buf.Bytes()
+
+	err = handleEvent(eventType, data)
+	check(err)
+}
+
+func Command(workdir, command string, args ...string) *exec.Cmd {
+	// log.Printf("wd = %s cmd = %s, args = %q", workdir, command, append([]string{}, args...))
+	cmd := exec.Command(command, args...)
+	cmd.Dir = workdir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd
+}
+
+const GIT_BASE_DIR = "repo"
+
+var (
+	ErrEmptyRepoName         = errors.New("Empty repository name")
+	ErrEmptyRepoOrganization = errors.New("Empty repository organization")
+)
+
+func eventPush(event PushEvent) (err error) {
+	if event.Repository.Name == "" {
+		return ErrEmptyRepoName
+	}
+	if event.Repository.Organization == "" {
+		return ErrEmptyRepoOrganization
+	}
+
 	ref := event.Ref
 	url := event.Repository.Url
 	after := event.After
@@ -133,16 +167,13 @@ func eventPush(event PushEvent) {
 	// The name of the subdirectory where the git
 	// mirror is (or will appear, if it hasn't been
 	// cloned yet).
-	url_base := path.Base(url)
-	git_dir := url_base
-	if !strings.HasSuffix(git_dir, ".git") {
-		git_dir = git_dir + ".git"
-	}
+	git_dir := path.Join(GIT_BASE_DIR, event.Repository.Organization,
+		event.Repository.Name)
 
-	clone := exec.Command("git", "clone", "--mirror", url)
-	clone.Stdout = os.Stdout
-	clone.Stderr = os.Stderr
-	err := clone.Run()
+	err = os.MkdirAll(git_dir, 0777)
+	check(err)
+
+	err = Command(".", "git", "clone", "-q", "--mirror", url, git_dir).Run()
 
 	if err == nil {
 		log.Println("Cloned", url)
@@ -150,12 +181,7 @@ func eventPush(event PushEvent) {
 	} else if _, ok := err.(*exec.ExitError); ok {
 
 		// Try "git remote update"
-		fetch := exec.Command("git", "fetch")
-		fetch.Dir = git_dir
-		fetch.Stdout = os.Stdout
-		fetch.Stderr = os.Stderr
-
-		err = fetch.Run()
+		err := Command(git_dir, "git", "fetch").Run()
 
 		if err != nil {
 			// git fetch where there is no update is exit status 1.
@@ -170,16 +196,18 @@ func eventPush(event PushEvent) {
 		check(err)
 	}
 
-	prefix_dir := url_base + "-" + after + "/"
+	prefix_dir := path.Join("checkout", after)
 
-	log.Println("Creating", prefix_dir)
-	archive := exec.Command("sh", "-c",
-		"(cd "+git_dir+"&& git archive --prefix="+
-			url_base+"-"+after+"/ "+after+
-			") | tar xvf -")
-	archive.Stderr = os.Stderr
-	err = archive.Run()
+	err = os.MkdirAll(path.Join(git_dir, prefix_dir), 0777)
+	check(err)
+
+	log.Println("Populating", prefix_dir)
+
+	args := []string{"--work-tree", prefix_dir, "checkout", after, "."}
+	err = Command(git_dir, "git", args...).Run()
 	check(err)
 
 	log.Println("Created", prefix_dir)
+
+	return
 }
