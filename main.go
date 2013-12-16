702597f9b0f71d3b3475c70792d6f19e1da63251
Peter Waller <peter@scraperwiki.com>
2013-12-16 16:14:27 +0000

    Move getListener out to its own file

--- main.go
+++ main.go
@@ -47,47 +47,6 @@ func check(err error) {
 	}
 }
 
-// Obtain listener by either taking it from `TANG_LISTEN_FD` if set, or
-// net.Listen otherwise.
-func getListener(address string) (l net.Listener, err error) {
-	var fd uintptr
-	if _, err = fmt.Sscan(os.Getenv("TANG_LISTEN_FD"), &fd); err == nil {
-		var listener_fd *os.File
-		listener_fd, err = InheritFd(fd)
-		if err != nil {
-			return
-		}
-
-		l, err = net.FileListener(listener_fd)
-		if err != nil {
-			err = fmt.Errorf("FileListener: %q", err)
-			return
-		}
-
-		return
-	}
-
-	l, err = net.Listen("tcp4", address)
-	if err != nil {
-		err = fmt.Errorf("unable to listen: %q", err)
-		return
-	}
-	log.Println("Listening on:", address)
-
-	fd = GetFd(l)
-	err = noCloseOnExec(fd)
-	if err != nil {
-		return
-	}
-
-	err = os.Setenv("TANG_LISTEN_FD", fmt.Sprintf("%d", fd))
-	if err != nil {
-		return
-	}
-
-	return
-}
-
 // Since CTRL-C is used for a reload, it's nice to have a way to exit (CTRL-D).
 func ExitOnEOF() {
 	func() {
