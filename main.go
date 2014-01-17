6cf333dadadf9569fcfe76d0a5e253de7c642f3e
Peter Waller <peter@scraperwiki.com>
2014-01-17 14:50:48 +0000

    Hi Ed

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func check(err error) {
 }
 
 func main() {
+	println("Hello, Ed!")
 	if tangRev == "" {
 		log.Println("tangRev and tangDate unavailable.")
 		log.Println("Use install-tang script if you want build date/version")
