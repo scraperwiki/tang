413093996f7405d4994794a83f2fa53cce33e294
Peter Waller <peter@scraperwiki.com>
2014-01-20 11:27:42 +0000

    Add trivial-repo fixture and test which builds it

--- main_test.go
+++ main_test.go
@@ -12,7 +12,35 @@ func init() {
 	}
 }
 
+func TestTrivialRepo(t *testing.T) {
+	cmd := Command("fixture", "tar", "xf", "trivial-repo.tar.bz2")
+	err := cmd.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := PushEvent{
+		Ref: "refs/heads/master",
+		Repository: Repository{
+			Name:         "trivial-repo",
+			Organization: "example",
+			Url:          "fixture/trivial-repo",
+		},
+		After:  "master",
+		Pusher: Pusher{Name: "testuser"},
+	}
+
+	allowedPushersSet["testuser"] = true
+	defer delete(allowedPushersSet, "testuser")
+
+	err = eventPush(e)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func TestPush(t *testing.T) {
+
 	e := PushEvent{
 		Ref: "refs/heads/master",
 		Repository: Repository{
@@ -23,28 +51,6 @@ func TestPush(t *testing.T) {
 		After:     "ee7c7b8f65dea5d3ef81c17eacd1b873be167109",
 		Pusher:    Pusher{Name: "testuser"},
 		NonGithub: NonGithub{NoBuild: true},
-	}
-
-	allowedPushersSet["testuser"] = true
-	defer delete(allowedPushersSet, "testuser")
-
-	err := eventPush(e)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestBuild(t *testing.T) {
-	e := PushEvent{
-		Ref: "refs/heads/master",
-		Repository: Repository{
-			Name:         "tang",
-			Organization: "example",
-			Url:          ".",
-		},
-		After:     "ee7c7b8f65dea5d3ef81c17eacd1b873be167109",
-		Pusher:    Pusher{Name: "testuser"},
-		// NonGithub: NonGithub{NoBuild: true},
 	}
 
 	allowedPushersSet["testuser"] = true
