55e32c7b80175ba0eb0d0bb0cf30bd7f5d893b47
Peter Waller <peter@scraperwiki.com>
2014-01-23 18:01:07 +0000

    Add code to clean up file descriptors when socket is closed

--- main.go
+++ main.go
@@ -200,6 +200,7 @@ func (ww *WebsocketWriter) Write(data []byte) (n int, err error) {
 
 func LiveLogHandler(response http.ResponseWriter, req *http.Request) {
 	ws, err := websocket.Upgrade(response, req, nil, 1024, 1024)
+	defer ws.Close()
 
 	if _, ok := err.(websocket.HandshakeError); ok {
 		http.Error(response, "Not a websocket handshake", 400)
@@ -211,26 +212,26 @@ func LiveLogHandler(response http.ResponseWriter, req *http.Request) {
 
 	stationaryFd, err := os.Open("/home/pwaller/test.log")
 	check(err)
+	defer stationaryFd.Close()
 	fd := follow.New(stationaryFd)
 
+	go func() {
+		var err error
+		// Wait until the other end closes the connection or sends
+		// a message.
+		t, m, err := ws.ReadMessage()
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("LiveLogHandler(): error reading msg: ", err)
+		}
+		_, _ = t, m
+		// Close the follow descripter, causes Copy to terminate
+		_ = fd.Close()
+	}()
+
 	w := &WebsocketWriter{ws}
-	n, err := io.Copy(w, fd)
-	log.Println("Err = ", err, n)
-
-	// ws.Write
-	// ws.NextWriter(messageType)
-	// ws.WriteJSON("Hello, world")
-
-	// w, err := ws.NextWriter(websocket.BinaryMessage)
-	// check(err)
-	// for i := 0; i < 1000; i++ {
-	// w.Write([]byte("Hello, world"))
-	// w.Close()
-
-	// ws.Wr
-	// }
-
-	_ = follow.New
+	// Blocks until web connection is closed.
+	_, err = io.Copy(w, fd)
+	// log.Println("Err =", err, n)
 }
 
 func ServeHTTP(l net.Listener) {
